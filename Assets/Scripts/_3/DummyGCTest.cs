432995040d36ec55a7607b558b29219b0cc9839d
VsPot07 <cocoacake84@gmail.com>
2023-07-19 15:27:02 +0900

    0719

---
+++ Assets/Scripts/_3/DummyGCTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyGCTest : MonoBehaviour
+{
+   
+    void Update()
+    {
+        CreateTanks();
+        DestroyTanks();
+    }
+
+    private void CreateTanks()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            GameObject tankObj = new GameObject();
+        }
+    }
+
+    private void DestroyTanks()
+    {
+        GameObject[] tankObjs = GameObject.FindGameObjectsWithTag("tank");
+        for (int i = 0; i < 100; i++)
+        {
+            Destroy(tankObjs[i]);
+        }
+    }
+}
---
+++ Assets/Scripts/_3/TankAI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankAI : MonoBehaviour
+{
+    Transform[] tanks;
+    public int numberOfTanks;
+    public GameObject tankPrefab;
+    GameObject player;
+
+    private void Start()
+    {
+        tanks = new Transform[numberOfTanks];
+
+        GameObject tank;
+
+        for (int i = 0; i < numberOfTanks; i++)
+        {
+            tank = Instantiate(tankPrefab);
+            tank.transform.position = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+
+            tanks[i] = tank.transform;
+        }
+    }
+
+    private void Update()
+    {
+        foreach(Transform t in tanks)
+        {
+            t.LookAt(player.transform.position);
+            t.Translate(0, 0, 0.05f);
+        }
+    }
+}
